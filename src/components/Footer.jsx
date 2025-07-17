56bdf7fdcdab1b297cd8c1370c710931b7312b61
Saurabh <saurabh.404dev@gmail.com>
2025-07-17 23:29:54 +0530

    home page completed ,made with love

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
+import { assets } from '../assets/assets';
 
 const Footer = () => {
   return (
-    <footer className="px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500">
+    <footer className="px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500 mt-20">
     <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
         <div className="md:max-w-96">
-            <img className="h-9" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoDark.svg" alt="dummyLogoDark"/>
+            <img className="h-9" src={assets.logo} alt="logo"/>
             <p className="mt-6 text-sm">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+               Experience the power of AI with jinxAI. <br /> Transform your content creation with our suite of premium AI tools. Write articles, generate images, and enhance your wokflow
             </p>
         </div>
         <div className="flex-1 flex items-start md:justify-end gap-20">
@@ -28,15 +27,21 @@ const Footer = () => {
                     <p>The latest news, articles, and resources, sent to your inbox weekly.</p>
                     <div className="flex items-center gap-2 pt-4">
                         <input className="border border-gray-500/30 placeholder-gray-500 focus:ring-2 ring-indigo-600 outline-none w-full max-w-64 h-9 rounded px-2" type="email" placeholder="Enter your email"/>
-                        <button className="bg-blue-600 w-24 h-9 text-white rounded">Subscribe</button>
+                        <button className="bg-[#444DFF] w-24 h-9 text-white rounded cursor-pointer">Subscribe</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <p className="pt-4 text-center text-xs md:text-sm pb-5">
-        Copyright 2024 © <a href="https://prebuiltui.com">PrebuiltUI</a>. All Right Reserved.
+        Copyright 2025 © <a href="https://prebuiltui.com">Saurabh</a>. All Right Reserved.
     </p>
+
+    <div className="w-full text-center py-4 text-gray-500 text-sm border-t mt-10">
+      Made with <span className="text-red-500">❤️</span> by Saurabh
+         
+    </div>
+
 </footer>
   )
 }
