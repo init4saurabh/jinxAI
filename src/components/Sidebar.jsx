ebec9fbf5b3a864805aa07a85dcad113e70dceea
Saurabh <saurabh.404dev@gmail.com>
2025-07-19 01:49:57 +0530

    menu section with navigated paths

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,19 @@
 import { useClerk, useUser } from '@clerk/clerk-react'
+import { Eraser, FileText, Hash, House, Icon, Image, Scissors, SquarePen, Users } from 'lucide-react';
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const navItems = [
+    {to:'/ai',label:'Dashboard', Icon: House},
+    {to:'/ai/write-article',label:'Write Article', Icon: SquarePen},
+    {to:'/ai/blog-titles',label:'Blog Titles', Icon: Hash},
+    {to:'/ai/generate-images',label:'Generate Images', Icon:Image},
+    {to:'/ai/remove-background',label:'Remove Background', Icon:Eraser},
+    {to:'/ai/remove-object',label:'Remove Object', Icon:Scissors},
+    {to:'/ai/review-resume',label:'Review Resume', Icon:FileText},
+    {to:'/ai/community',label:'Community', Icon:Users},
+    
+]
 
 const Sidebar = ({sidebar,setSideBar}) => {
 
@@ -11,6 +25,23 @@ const Sidebar = ({sidebar,setSideBar}) => {
         <div className='my-7 w-full'>
             <img src={user.imageUrl} alt="User avatar" className='w-13 rounded-full mx-auto'/>
             <h1 className='mt-1 text-center'>{user.fullName}</h1>
+            <div className='px-6 mt-5 text-sm text-gray-600 font-medium'>
+               {navItems.map(({ to, label, Icon }) => (
+                        <NavLink
+                            key={to}
+                            to={to}
+                            end={to === '/ai'}
+                            onClick={() => setSideBar(false)}
+                            className={({ isActive }) =>
+                            `px-3.5 py-2.5 flex items-center gap-3 rounded ${isActive ? 'bg-gradient-to-r from-[#3C81F6] to-[#9234EA] text-white' : ''}`
+                            }
+                        >
+                            <Icon className={`w-4 h-4`} />
+                            {label}
+                        </NavLink>
+                        ))}
+
+            </div>
         </div>
 
 
