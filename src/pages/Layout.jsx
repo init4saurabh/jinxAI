fbe73be11d968aa155e08e568c7b6f6db6421419
Saurabh <saurabh.404dev@gmail.com>
2025-07-18 23:57:37 +0530

    dashboard

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const Layout = () => {
+
+  const navigate = useNavigate();
+  const [sidebar, setSidebar] = useState(false)
   return (
-    <div>
-        <h1>Layout</h1>
+    <div className='flex flex-col items-start justify-start h-screen'>
+       <nav>
+        <img src={assets.logo} alt="" onClick={()=>navigate('/')} />
+        
+        
+       </nav>
         <Outlet/>
     </div>
   )
