0b4b8b62b96257003fba8b6d46fe9ae65461f2e8
Saurabh <saurabh.404dev@gmail.com>
2025-08-02 22:36:00 +0530

    fix

--- src/pages/WriteArticle.jsx
+++ src/pages/WriteArticle.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const WriteArticle = () => {
   const articleLength = [
     {length: 800,text: 'Short(500-800 words)'},
-    {length: 800,text: 'Short(500-800 words)'}
+    {length: 1200,text: 'Medium(800-1200 words)'},
+    {length: 1600,text: 'Long(1200-1600 words)'}
   ]
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
@@ -15,11 +16,17 @@ const WriteArticle = () => {
         <p className='mt-6 text-sm font-medium'>Article Topic</p>
         <input type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='The future of artificial intelligence is ...' required />
         <p className='mt-4 text-sm font-medium'>Article Length</p>
+
+        <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+          {articleLength.map((item,index)=>(
+            <span key={index}>{item.text}</span>
+          ))}
+        </div>
       </form>
 
       {/*right col */}
       <div>
-
+            .
       </div>
         
     </div>
