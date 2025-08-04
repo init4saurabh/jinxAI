8c4725292e2ade606dc2f0ac4e931c1f57a1d2e1
Saurabh <saurabh.404dev@gmail.com>
2025-08-04 23:26:13 +0530

    add

--- src/pages/WriteArticle.jsx
+++ src/pages/WriteArticle.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const WriteArticle = () => {
   const articleLength = [
     {length: 800,text: 'Short(500-800 words)'},
     {length: 1200,text: 'Medium(800-1200 words)'},
     {length: 1600,text: 'Long(1200-1600 words)'}
+
+    const [selectedLength, setselectedLength] = useState(articleLength[0])
+    const [input,setInput] = useState('')
+
   ]
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
@@ -19,7 +23,7 @@ const WriteArticle = () => {
 
         <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
           {articleLength.map((item,index)=>(
-            <span key={index}>{item.text}</span>
+            <span onClick={()=> setselectedLength(item)} className = {`text-xs px-4 py-1 border rounded-full  cursor-pointer`}key={index}>{item.text}</span>
           ))}
         </div>
       </form>
