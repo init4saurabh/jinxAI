9fbf06eae103712e73b79101585ac1b2e7db31e8
Saurabh <saurabh.404dev@gmail.com>
2025-08-11 02:01:33 +0530

    review-resume page

--- src/pages/RemoveObject.jsx
+++ src/pages/RemoveObject.jsx
@@ -21,7 +21,7 @@ const RemoveObject = () => {
      
     <p className='mt-6 text-sm font-medium'>Describe object name to remove</p>
 
-        <textarea onChange={(e)=>setObject(e.target.value)} value ={object} rows ={4}className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder='e.g ,, watch or glass, only single object name' required />
+        <textarea onChange={(e)=>setObject(e.target.value)} value ={object} rows ={4}className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300'  required />
 
         <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417D6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
           <Scissors className='w-5'/>
@@ -39,8 +39,8 @@ const RemoveObject = () => {
         </div>
         <div className='flex-1 flex justify-center items-center'>
           <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
-             <Eraser className='w-9 h-9]'/>
-             <p>Upload an image and click "RemoveBackground" to get started</p>
+             <Scissors className='w-9 h-9]'/>
+             <p>Upload an image and click "Remove Object" to get started</p>
 
           </div>
 
